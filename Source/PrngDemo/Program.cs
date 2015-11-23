b9b3bfe11c1ec1d4cf9be495dedcd681452f7cb2
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2015-11-22 23:53:24 -0500

    start decoupling app from form

--- Source/PrngDemo/Program.cs
+++ Source/PrngDemo/Program.cs
@@ -3,17 +3,33 @@ using System.Windows.Forms;
 
 namespace PrngDemo
 {
-   static class Program
+   // The async pattern here is based on:
+   // https://msdn.microsoft.com/en-us/magazine/mt620013
+   public class Program
    {
+      private readonly Form1 _form1;
+
       /// <summary>
       /// The main entry point for the application.
       /// </summary>
       [STAThread]
       static void Main()
       {
+         var program = new Program();
+         program.Start();
+      }
+     
+      private Program()
+      {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault( false );
-         Application.Run( new Form1() );
+         _form1 = new Form1();
+
+      }
+
+      public void Start()
+      {
+         Application.Run( _form1 );
       }
    }
 }
